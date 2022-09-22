235e6ef3ad4d2d5e9f55386eff5e3a5c03dab120
CKS <communicate2charles@gmail.com>
2022-09-22 15:30:14 +0700

    templates dynamic routing

--- codexdiscord/base/views.py
+++ codexdiscord/base/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+rooms = [
+    {'id': 1, 'name': 'Python shcool'},
+    {'id': 2, 'name': 'DJANGO DISC'},
+    {'id': 3, 'name': 'hacking'},
+    {'id': 4, 'name': 'linux'},
+    {'id': 5, 'name': 'recursive loop'},
+]
+
+
+def home(request):
+    #as dictionary we pass variables. key is a name of variable, value is a reference to it
+    context = {'rooms': rooms}
+    return render(request, 'base/home.html', context)
+
+
+def room(request, pk):
+    room = None
+    for i in rooms:
+        if i['id'] == int(pk):
+            room = i
+    context = {'room': room}
+    return render(request, 'base/room.html', context)
+
+
+
