9afb4094f769a5e3342d639fae36b6e0e3e85772
CKS <communicate2charles@gmail.com>
2022-09-22 21:59:04 +0700

    edit room by update room. using insance in room_form

--- codexdiscord/base/views.py
+++ codexdiscord/base/views.py
@@ -3,15 +3,6 @@ from .models import Rooms
 from .forms import RoomForm
 
 
-# rooms = [
-#     {'id': 1, 'name': 'Python shcool'},
-#     {'id': 2, 'name': 'DJANGO DISC'},
-#     {'id': 3, 'name': 'hacking'},
-#     {'id': 4, 'name': 'linux'},
-#     {'id': 5, 'name': 'recursive loop'},
-# ]
-
-
 def home(request):
     rooms = Rooms.objects.all()
     #as dictionary we pass variables. key is a name of variable, value is a reference to it
@@ -32,6 +23,21 @@ def create_room(request):
         if form.is_valid():
             form.save()
             return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def update_room(request, pk):
+    room = Rooms.objects.get(id=pk)
+    form = RoomForm(instance=room)
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
