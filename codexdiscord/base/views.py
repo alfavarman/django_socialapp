28f8af6446c4cd335133d0dc096083eaca316699
CKS <communicate2charles@gmail.com>
2022-09-22 18:54:24 +0700

    query database for rooms

--- codexdiscord/base/views.py
+++ codexdiscord/base/views.py
@@ -1,28 +1,25 @@
 from django.shortcuts import render
+from .models import Rooms
 
 
-rooms = [
-    {'id': 1, 'name': 'Python shcool'},
-    {'id': 2, 'name': 'DJANGO DISC'},
-    {'id': 3, 'name': 'hacking'},
-    {'id': 4, 'name': 'linux'},
-    {'id': 5, 'name': 'recursive loop'},
-]
+# rooms = [
+#     {'id': 1, 'name': 'Python shcool'},
+#     {'id': 2, 'name': 'DJANGO DISC'},
+#     {'id': 3, 'name': 'hacking'},
+#     {'id': 4, 'name': 'linux'},
+#     {'id': 5, 'name': 'recursive loop'},
+# ]
 
 
 def home(request):
+    rooms = Rooms.objects.all()
     #as dictionary we pass variables. key is a name of variable, value is a reference to it
     context = {'rooms': rooms}
     return render(request, 'base/home.html', context)
 
 
 def room(request, pk):
-    room = None
-    for i in rooms:
-        if i['id'] == int(pk):
-            room = i
+    room = Rooms.objects.get(id=pk)
     context = {'room': room}
     return render(request, 'base/room.html', context)
-
-
 
